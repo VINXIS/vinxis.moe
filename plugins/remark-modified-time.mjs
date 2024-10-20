b20e70b73682c1db16748355cc795e4b0bb3da0e
VINXIS <oykxf2@gmail.com>
2024-10-19 20:41:36 -0600

    change date time format for posts

--- plugins/remark-modified-time.mjs
+++ plugins/remark-modified-time.mjs
@@ -4,6 +4,13 @@ export function remarkModifiedTime () {
     return function (tree, file) {
         const filepath = file.history[0];
         const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-        file.data.astro.frontmatter.lastModified = result.toString();
+        file.data.astro.frontmatter.lastModified = new Intl.DateTimeFormat("en-GB", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+        }).format(new Date(result.toString())).replace(",", "");
     };
 }
