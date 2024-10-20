0e655f17427c33a8cc7241c7ac62b80b296b61cd
VINXIS <oykxf2@gmail.com>
2024-10-20 14:37:41 -0600

    attempting to use stat instead as the placeholder and added created time

--- plugins/remark-modified-time.mjs
+++ plugins/remark-modified-time.mjs
@@ -1,16 +1,14 @@
-import { execSync } from "child_process";
+import { stat } from "fs";
+import dateToString from "../src/utils/dateToString";
 
 export function remarkModifiedTime () {
     return function (tree, file) {
-        const filepath = file.history[0];
-        const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-        file.data.astro.frontmatter.lastModified = new Intl.DateTimeFormat("en-GB", {
-            year: "numeric",
-            month: "short",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-        }).format(new Date(result.toString())).replace(",", "");
+        stat(file.history[0], (err, stats) => {
+            if (err)
+                throw err;
+
+            file.data.astro.frontmatter.lastModified = dateToString(file.data.astro.frontmatter.lastModified ?? stats.mtime);
+            file.data.astro.frontmatter.created = dateToString(file.data.astro.frontmatter.created ?? stats.birthtime);
+        });
     };
 }
