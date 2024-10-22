757852b6fab3a87ce7dfffb78f6b459b37821678
VINXIS <oykxf2@gmail.com>
2024-10-22 17:28:32 -0600

    filegraph component + url parsing fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import remarkWikiLink from "remark-wiki-link";
 const files = import.meta.glob("./src/content/**/*.md");
 const permalinks = [];
 for (const file in files)
-    permalinks.push(file.split("/").pop().replace(".md", "").replace(/ /g, "-").toLowerCase());
+    permalinks.push(file.split("/").pop().replace(".md", "").replace(/\p{punct}/gu, "").replace(/ /g, "-").toLowerCase());
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,7 +41,7 @@ export default defineConfig({
                 remarkWikiLink,
                 {
                     permalinks,
-                    pageResolver: (name) => [name.replace(/ /g, "-").toLowerCase()],
+                    pageResolver: (name) => [name.replace(/\p{punct}/gu, "").replace(/ /g, "-").toLowerCase()],
                     hrefTemplate: (permalink) => `../notes/${permalink}`,
                     newClassName: "invalid-internal-link",
                 },
---
+++ src/utils/pageInfo.ts
@@ -0,0 +1,11 @@
+import type { CollectionKey } from "astro:content";
+
+export interface BasePageInfo {
+    name: string;
+    slug: string;
+    collection: CollectionKey;
+}
+
+export type PageLink = Omit<BasePageInfo, "collection"> & {
+    collection: CollectionKey | undefined;
+}
